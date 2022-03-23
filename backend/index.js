36ed37d386f6e377a0ebf05534e797b4a356ffa4
Jason Zissman <jason.zissman@gmail.com>
2022-03-23 06:54:13 -0500

    Include full React SPA

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import apiRouter from "./backend/routes/widgets.js";
+import apiRouter from "./routes/widgets.js";
 
 const app = express();
 const port = 3000;
+const STATIC_FILE_DIR = process.env.STATIC_FILE_DIR || '..\\frontend\\build';
 
 app.get('/health', (req, res) => {
     res.json({ health_check: "ok" });
@@ -10,7 +11,7 @@ app.get('/health', (req, res) => {
 
 app.use('/api', apiRouter);
 
-app.use(express.static('ui'));
+app.use(express.static(STATIC_FILE_DIR));
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
